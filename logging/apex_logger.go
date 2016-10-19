714ab495ac258bd77afd03c735021c53af589c7c
Francois Hill (home win10) <francoishill11@gmail.com>
2016-10-19 19:24:47 +0200

    fix: add explicit flag in constructor for `exitOnEmergency`

--- logging/apex_logger.go
+++ logging/apex_logger.go
@@ -2,6 +2,7 @@ package logging
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
 	apex "github.com/francoishill/log"
@@ -10,28 +11,36 @@ import (
 )
 
 //NewApexLogger creates a new logger
-func NewApexLogger(level apex.Level, handler apex.Handler, apexEntry *apex.Entry) Logger {
+func NewApexLogger(level apex.Level, handler apex.Handler, apexEntry *apex.Entry, exitOnEmergency bool) Logger {
 	//TODO: This set level and handler is global, this should not happen inside `NewApexLogger`
 	apex.SetLevel(level)
 	apex.SetHandler(handler)
 
 	return &apexLogger{
-		level:          level,
-		handler:        handler,
-		apexEntry:      apexEntry,
-		errStackTraces: true, //TODO: Is this fine by default?
+		level:           level,
+		handler:         handler,
+		apexEntry:       apexEntry,
+		errStackTraces:  true, //TODO: Is this fine by default?
+		exitOnEmergency: exitOnEmergency,
 	}
 }
 
 type apexLogger struct {
-	level          apex.Level
-	handler        apex.Handler
-	apexEntry      *apex.Entry
-	errStackTraces bool
+	level           apex.Level
+	handler         apex.Handler
+	apexEntry       *apex.Entry
+	errStackTraces  bool
+	exitOnEmergency bool
 }
 
 func (l *apexLogger) Emergency(s string) {
-	l.apexEntry.Fatalf(s)
+	if l.exitOnEmergency {
+		l.apexEntry.Fatalf(s)
+		//Sure this is not required since apex Entry.Fatal already calls `os.Exit(1)`. But this is for "safety" if that ever changes in apex
+		os.Exit(1)
+	} else {
+		l.apexEntry.Errorf(s)
+	}
 }
 func (l *apexLogger) Alert(s string) {
 	l.apexEntry.Errorf(s)
@@ -67,15 +76,15 @@ func (l *apexLogger) WithError(err error) Logger {
 		stack := strings.Replace(debug.GetFullStackTrace_Normal(false), "\n", "\\n", -1)
 		newEntry = newEntry.WithField("stack", stack)
 	}
-	return NewApexLogger(l.level, l.handler, newEntry)
+	return NewApexLogger(l.level, l.handler, newEntry, l.exitOnEmergency)
 }
 
 func (l *apexLogger) WithField(key string, value interface{}) Logger {
-	return NewApexLogger(l.level, l.handler, l.apexEntry.WithField(key, value))
+	return NewApexLogger(l.level, l.handler, l.apexEntry.WithField(key, value), l.exitOnEmergency)
 }
 
 func (l *apexLogger) WithFields(fields map[string]interface{}) Logger {
-	return NewApexLogger(l.level, l.handler, l.apexEntry.WithFields(apex.Fields(fields)))
+	return NewApexLogger(l.level, l.handler, l.apexEntry.WithFields(apex.Fields(fields)), l.exitOnEmergency)
 }
 
 func (l *apexLogger) DeferredRecoverStack(debugMessage string) {
